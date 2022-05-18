dc133a2894fa8eb990d778a6b60a689c49060786
Natalia Ferreira <nataliaferreira.dev@yahoo.com>
2022-05-17 21:17:32 -0300

    Inclusao do Menu Principal

--- src/gui/MenuPrincipalController.java
+++ src/gui/MenuPrincipalController.java
@@ -1,56 +1,85 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
 
 public class MenuPrincipalController implements Initializable {
 
 	@FXML
 	private MenuItem menuItemCadastrar;
-	
+
 	@FXML
 	private MenuItem menuItemAtualizar;
 	
 	@FXML
 	private MenuItem menuItemListar;
-	
+
 	@FXML
 	private MenuItem menuItemPesquisar;
-	
+
 	@FXML
 	private MenuItem menuItemSobre;
 	
+
 	@FXML
 	public void onMenuItemCadastrarAction() {
-		System.out.println("Cadastrado com sucesso!!!");
+		System.out.println("Cadastrado!!!!");
 	}
-	
+
 	@FXML
 	public void onMenuItemAtualizarAction() {
-		System.out.println("Atualizado com sucesso!!!");
+		System.out.println("Atualizado");
 	}
 	
 	@FXML
 	public void onMenuItemListarAction() {
-		System.out.println("Listar todos!!!");
+		System.out.println("Lista de Sabres");
 	}
 	
 	@FXML
 	public void onMenuItemPesquisarAction() {
-		System.out.println("Pesquisar sabre!!!");
+		System.out.println("Pesquisando....");
 	}
-	
+
 	@FXML
 	public void onMenuItemSobreAction() {
-		System.out.println("Sobre!!!");
+		loadView("/gui/Sobre.fxml");
 	}
-	
+
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 	}
-	
+
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVbox.getChildren().get(0);
+			mainVbox.getChildren().clear();
+			mainVbox.getChildren().add(mainMenu);
+			mainVbox.getChildren().addAll(newVBox.getChildren());
+			
+		} catch (IOException ex) {
+			Alerts.showAlert("IOException", "Error loading view", ex.getMessage(), AlertType.ERROR);
+		}
+	}
+
 }
